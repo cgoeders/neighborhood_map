9c746ddb8d11571204c80c4cb33a73fe5e91acb8
Christina Goeders <xgoeders@gmail.com>
2015-11-04 15:01:50 -0800

    Housekeeping (removed console.log statements, added a few extra comments).

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,16 @@
-//initialize variables
+/*
+
+This is the main file running the application. This site allows users to
+search either a list view or a map marker view for a coffee shop among 
+10 given locations in Palo Alto, CA. The app utilizes both Google Maps JavaScript
+API and Foursquare's API to display information about each marker
+location in an information window upon user click. Code has been organized 
+according to KnockoutJS's MVVM design pattern.
+
+*/
+
+
+//initialize global variables
 var map, 
 	mapCanvas, 
 	mapOptions, 
@@ -9,77 +21,67 @@ var map,
 	infoWindow;
 
 
-//array of nearby coffee locations
+//define array of nearby coffee locations
 var cafeList = [
 	{
 		name: 'ZombieRunner',
 		lat: 37.4259801,
 		lng: -122.1469309,
 		markerIndex: 0,
-		show: true
 	},
 	{
 		name: 'Philz Coffee (Midtown)',
 		lat: 37.4295836,
 		lng: -122.1249023,
 		markerIndex: 1,
-		show: true
 	},
 	{
 		name: 'Blue Bottle Coffee',
 		lat: 37.4475823,
 		lng: -122.1618107,
 		markerIndex: 2,
-		show: true
 	},
 	{
 		name: 'Starbucks (Stanford)',
 		lat: 37.4241183,
 		lng: -122.1730281,
 		markerIndex: 3,
-		show: true
 	},
 	{
 		name: 'Peet\'s Coffee & Tea',
 		lat: 37.4380821,
 		lng: -122.1615256,
 		markerIndex: 4,
-		show: true
 	},
 	{
 		name: 'Coupa Cafe (Downtown)',
 		lat: 37.44468,
 		lng: -122.1637195,
 		markerIndex: 5,
-		show: true
 	},
 	{
 		name: 'Cafe Venetia',
 		lat: 37.4474572,
 		lng: -122.162427,
 		markerIndex: 6,
-		show: true
 	},
 	{
 		name: 'La Baguette',
 		lat: 37.4429066,
 		lng: -122.1739064,
 		markerIndex: 7,
-		show: true
 	},
 	{
 		name: 'Philz Coffee (Downtown)',
 		lat: 37.4421512,
 		lng: -122.163721,
 		markerIndex: 8,
-		show: true
 	},
 	{
 		name: 'Starbucks (Midtown)',
 		lat: 37.4332611,
 		lng: -122.1309018,
 		markerIndex: 9,
-		show: true
 	}
 ];
 
@@ -95,44 +97,27 @@ var ViewModel = function() {
 
 	self.currentLocation = ko.observable('Palo Alto, CA');
 	
+	//make KO observables
 	self.inputQuery = ko.observable('');
 
 	self.cafeList = ko.observableArray(cafeList);
 
 	self.markerList = ko.observableArray([]);
+	
 	self.filterList = ko.observableArray([]);
 
 	self.notifyUser = ko.observable('Welcome!');
 
-	//TO FIX!!!!!!!!!!
-	(function() {
-		if (self.filterList.length > 0) {
-			return 'Showing results matching your search...';
-		}
-		return '';
-	});
-
-	// self.showLiveInput = ko.computed(function() {
- //  		console.log(self.inputQuery());
- //  		self.runFilter;
-	// });
-
-
-
 	//filter list and markers based on user's input query
 	self.runFilter = function() {
 		var query = self.inputQuery().toLowerCase();
-		console.log(query);
-		// self.inputQuery(self.inputQuery().toLowerCase());
-		// console.log(self.inputQuery());
 
-		//TODO (for live search): make all existing markers invisible as user types
-
+		//make all markers initially invisible
 		self.filterList().forEach(function(markerItem) {
 			markerItem.setVisible(false);
 		});
 
-		//update `filterList` with filtered markers using query
+		//update `filterList` with filtered markers using `query`, and display relevant markers
 		self.filterList(ko.utils.arrayFilter(self.markerList(), function(item) {
 			if (item.title.toLowerCase().indexOf(query) >= 0) {
 				item.setVisible(true);
@@ -141,13 +126,16 @@ var ViewModel = function() {
 			return false;
 		}));
 
+		//notify user of search results
 		if (self.filterList().length === 0) {
 			self.notifyUser('No results. Try another search term.');
+		} else if (self.filterList().length === 1) {
+			self.notifyUser('Showing ' + self.filterList().length.toString() + ' result');
 		} else {
-			self.notifyUser('Showing ' + self.filterList().length.toString() + ' result(s)');
+			self.notifyUser('Showing ' + self.filterList().length.toString() + ' results');
 		};
 
-		//if only one item in `filterList`, zoom to marker and openInfoWindow
+		//if only one item in `filterList`, zoom to that marker and open an info window
 		if (self.filterList().length === 1) {
 			var onlyItem = self.filterList()[0];
 			self.openInfoWindow(onlyItem);
@@ -157,12 +145,9 @@ var ViewModel = function() {
 			map.setZoom(13);
 			infoWindow.close();
 		}
-
-		console.log(self.filterList(), 'filterList');
-		console.log(self.markerList(), 'markerList');
 	};
 
-
+	//useful to convert marker item to simple place item
 	self.getPlaceFromMarker = function(markerItem) {
 		var placeItem;
 		for (var i = 0; i < cafeList.length; i++) {
@@ -172,8 +157,6 @@ var ViewModel = function() {
 		}
 		return placeItem;
 	}
-
-
 
  	//initialize Google Map
  	var initialize = function() {
@@ -190,13 +173,8 @@ var ViewModel = function() {
  		};
  		map = new google.maps.Map(mapCanvas, mapOptions);
 
- 		//add map markers for venues in `cafeList`
+ 		//add map markers for all venues in `cafeList`
  		self.cafeList().forEach(function(place) {
-
- 			// console.log(place);
- 			// console.log(callFoursquare(place));
- 			// console.log(place.FSvenueName);
-
  			markerOptions = {
  				position: new google.maps.LatLng(place.lat, place.lng),
  				map: map,
@@ -208,18 +186,20 @@ var ViewModel = function() {
  			};
 
  			marker = new google.maps.Marker(markerOptions);
+
+ 			//keep track of markers
  			self.markerList().push(marker);
  			self.filterList(self.markerList());
 
  			//add info window
  			infoWindow = new google.maps.InfoWindow({
- 				content: 'testing123'
+ 				content: ''
  			});
 
  			//add marker click handler
  			google.maps.event.addListener(marker, 'click', (function(marker) {
  				return function() {
- 					//make map center on selected marker
+ 					//center map on selected marker
  					center = marker.getPosition();
  					map.setZoom(14);
  					map.panTo(center);
@@ -239,15 +219,12 @@ var ViewModel = function() {
 		google.maps.event.addDomListener(window, 'resize', function() {
 			map.panTo(center);
 		});
-
- 		// console.log(self.markerList());
- 		// console.log(self.filterList());
- 		// console.log(center);
  	}
 
-
+ 	//populates info window with requested Foursquare data
 	self.openInfoWindow = function(listItem) {
         
+        //info needed to make Foursquare request
         var placeObj = self.getPlaceFromMarker(listItem);
  		var markLat = placeObj.lat;
  		var markLng = placeObj.lng;
@@ -270,52 +247,49 @@ var ViewModel = function() {
  				  '&client_secret=' + clientSecret + 
  				  '&query=' + queryName;
 
+ 		//request data from Foursquare
 		$.ajax({
 			url: url,
 			dataType: 'json',
 			success: function(data) {
 				venue = data.response.venues[0];
-				console.log(venue);
+
+				//info to be displayed in info window
 				venueName = venue.name; 
 				venuePhone = venue.contact.formattedPhone;
 				venueURL = venue.url;
 
-                                center = listItem.getPosition();
-                                map.setZoom(14);
-                                map.panTo(center);
+				//center the map on this item
+                center = listItem.getPosition();
+                map.setZoom(14);
+                map.panTo(center);
 
-                                //animate marker with bounce; set timeout for bounce
-                                listItem.setAnimation(google.maps.Animation.BOUNCE);
-                                setTimeout(function() {
-                                  listItem.setAnimation(null);
-                                }, 2150);
-                                
-                                infoWindow.open(map, listItem);
-                                infoWindow.setContent('<div id="info-content"></div>');
+                //animate marker with bounce; set timeout for bounce
+                listItem.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function() {
+                  listItem.setAnimation(null);
+                }, 2150);
+                
+                //open info window
+                infoWindow.open(map, listItem);
+                infoWindow.setContent('<div id="info-content"></div>');
 
+                //add information to info window
 				$('#info-content').append('<p><strong>' + venueName + '</strong></p>');
 				$('#info-content').append('<p><strong>Phone: </strong>' + venuePhone + '</p>');
 				$('#info-content').append('<p><strong>Website: </strong><a href="' + venueURL + '">' + venueURL + '</p>');
 			},
+			//notify user if error occurs with request
 			error: function() {
-				console.log("OH NO -- ERROR");
+				console.log("AN ERROR HAS OCCURRED");
 				alert("Error! Foursquare data request failed.");
 				self.notifyUser('Foursquare data could not be loaded.');
 			}
 		});
-
  	};
 
-	// //TODO: create user error message in case GMaps doesn't load
-	// if (typeof google !== 'object' || typeof google.maps !== 'object') {
-	// 	self.notifyUser("Google Maps could not be loaded");
-	// }
-
+ 	//on load, run the main `initialize` function
 	google.maps.event.addDomListener(window, 'load', initialize);
-
 };
 
 ko.applyBindings(new ViewModel());
-
-
-
